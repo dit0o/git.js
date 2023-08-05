478e681736033730633e8c632daaab724f1a4cb8
dit0o <gaganam6985@gmail.com>
2023-08-05 12:28:17 +0530

    chat 3

--- controllers/msg.js
+++ controllers/msg.js
@@ -1,7 +1,8 @@
 const Chat=require('../model/chat')
+const User=require('../model/user')
 exports.postMsg=async(req,res,next)=>{
     try {
-        const{id,Name}=req.user;
+        const{id}=req.user;
         const message=req.body.message;
         const mesg = await Chat.create( {message, userId:id});
         res.status(200).json({mesg, Name});
@@ -13,10 +14,14 @@ exports.postMsg=async(req,res,next)=>{
 }
 exports.getMessage = async (req,res,next)=>{
     try{
-    const {id, Name} = req.user;
 
-    const mesg = await Chat.findAll({where: {userId:id}});
-    res.status(200).json({mesg, Name});
+    const mesg = await Chat.findAll({include:[
+{
+    model:User,
+    attributes: ['id', 'Name']
+
+}] });
+    res.status(200).json({mesg});
     } catch(error) {
         console.log(error);
         res.status(500).json({error,success: false});
--- routes/message.js
+++ routes/message.js
@@ -3,5 +3,5 @@ const msgController=require('../controllers/msg')
 const auth=require('../middleware/auth')
 const router=express.Router();
 router.get('/user/message',auth.authentication,msgController.getMessage)
-router.post('/user/postmeg',auth.authentication,msgController.postMsg)
+router.post('/user/message',auth.authentication,msgController.postMsg)
 module.exports=router;
