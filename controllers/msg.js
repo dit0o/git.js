0ddd5124271dc8201139022f90ab8da999b7a343
dit0o <gaganam6985@gmail.com>
2023-08-11 21:53:59 +0530

    making group

--- controllers/msg.js
+++ controllers/msg.js
@@ -1,10 +1,15 @@
 const Chat=require('../model/chat')
 const User=require('../model/user')
+const Group=require('../model/group')
+const Groupmembers=require('../model/membergroup')
+const jwt = require('jsonwebtoken');
 exports.postMsg=async(req,res,next)=>{
     try {
         const{id}=req.user;
         const message=req.body.message;
-        const mesg = await Chat.create( {message, userId:id});
+        const gId=req.query.gId;
+        console.log(gId)
+        const mesg = await Chat.create( {message, userId:id,groupId:gId});
         res.status(200).json({mesg, Name});
         
     } catch (error) {
@@ -16,6 +21,8 @@ exports.getMessage = async (req,res,next)=>{
     try{
      
         const lastId=+req.query.lastId || 0;
+        const gId = +req.query.gId;
+        console.log(gId)
         console.log(lastId)
     let mesg = await Chat.findAll({offset:lastId ,include:[
 {
@@ -23,10 +30,46 @@ exports.getMessage = async (req,res,next)=>{
     attributes: ['id', 'Name']
 
 }
-]});
+],where:{
+    groupId:gId
+}
+});
     res.status(200).json({mesg});
     } catch(error) {
         console.log(error);
         res.status(500).json({error,success: false});
+    }
+}
+exports.postCreatgroup=async(req,res,next)=>{
+    try {
+        const eId=req.user.id;
+        const {gName}=req.body;
+        const newGroup=await Group.create({gName,userId:eId});
+        const nGId=newGroup.dataValues.id;
+        const groupmem=await Groupmembers.create({userId:eId,groupId:nGId})
+        res.status(200).json({newGroup,groupmem,success:true});
+    }catch(error){
+        console.log(error);
+        res.status(500).json({error,success:false});
+    }
+        
+  
+}
+exports.getAllG = async(req,res,next) => {
+    try {
+        const reqId = req.user.id;
+        let allGroup = await Groupmembers.findAll({
+            include: [{
+                model: Group,
+                attributes: ['gName']
+            }]
+            ,where: {
+                userId: reqId
+            }
+        });
+        res.status(200).json({allGroup});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error,success:false});
     }
 }
--- routes/message.js
+++ routes/message.js
@@ -4,4 +4,6 @@ const auth=require('../middleware/auth')
 const router=express.Router();
 router.get('/user/message',auth.authentication,msgController.getMessage)
 router.post('/user/message',auth.authentication,msgController.postMsg)
+router.post('/user/message/creategroup',auth.authentication,msgController.postCreatgroup)
+router.get('/user/message/allgroup',auth.authentication,msgController.getAllG)
 module.exports=router;
